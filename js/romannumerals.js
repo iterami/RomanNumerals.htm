06cbf277f8ab89c49b1f2374fcf9d10e583c4cc0
honzi <czechinthemail@gmail.com>
2017-06-08 14:08:08 -0700

    added core_repo_init() usage

--- js/romannumerals.js
+++ js/romannumerals.js
@@ -40,6 +40,10 @@ function handle_input(){
 }
 
 function repo_init(){
+    core_repo_init({
+      'title': 'RomanNumerals.htm',
+    });
+
     document.getElementById('input').oninput = handle_input;
 }
 
